568d6871842d3d5459db01bc0f878bcb8fd72f97
Bogdan003 <bogdanh_2007@yahoo.com>
2023-10-13 18:42:13 +0300

    added new testings

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$filename = '2023-10-04.json';
+$filename = '2023-10-13.json';
 $jsonContent = file_get_contents('../input/' . $filename);
 $jsonData = json_decode($jsonContent, true);
 
@@ -8,8 +8,5 @@ require_once '../controller/crosswordsClass.php';
 $crossWordsObj = new Crosswords($jsonData);
 $tableDimensions = $crossWordsObj->getTable();
 $additionalCellsData = $crossWordsObj->getAdditionalCellsData();
-
-// echo "<pre>";
-// var_dump($additionalCellsData);
 
 require_once '../views/view.php';
